006892ab6f78a6cb65aaffaff832515a4a1ea5b3
dodiforth <dowoniscool@yahoo.com>
2022-06-23 11:19:56 +0000

    Add Logical Operators challenge file.

---
+++ Programming in Swift Fundamentals/episode06Challenge Logical Operators.swift
@@ -0,0 +1,34 @@
+/*
+Episode 06 : Challenge - Logical Operators
+
+Challenge 1
+
+You've been provided with a constant names myAge below that's already been assigned a vlaue.
+Feel free to change the value of this constant tomatch your actual age.
+
+Use that constant to create an if-else statement to print out "Teenager" if the value of myAge is 
+greater or than 13 but less than or equal to 19, and to print out "Not a teenager" if the value is
+outside that range.
+
+*/
+
+import Foundation 
+
+let myAge = 28
+
+if 13 <= myAge && myAge <= 19 {
+	print("Teenager")
+} else {
+	print("Not a teenager")
+}
+
+/*
+Challenge 2
+
+Create a constant named teenagerName, and use a ternary conditional operator to set the value
+of teenagerName to your own name as a string if the value of myAge, declared above,
+is greater than or equal to 13, but less than or equal to 19, and to set the value of 
+teenagerName to "Not me!" if the value is outside that range.
+
+Then print out the value of teenagerName.
+*/
