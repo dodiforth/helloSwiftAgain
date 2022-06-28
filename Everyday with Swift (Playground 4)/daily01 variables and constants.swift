a77d9a2932a469ecaca95bdff602fcf8fba1220a
dodiforth <dowoniscool@yahoo.com>
2022-06-28 00:36:51 +0000

    Create a new folder for learning Swift

---
+++ Everyday with Swift (Playground 4)/daily01 variables and constants.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+print("I have learned the following : ")
+print("What features make Swift a modern and safe language")
+print("How to use the Swift REPL in Terminal")
+print("How to use playgrounds to make writing Swift fun ans simple")
