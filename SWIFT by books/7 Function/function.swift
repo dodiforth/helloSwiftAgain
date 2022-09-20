6fb62e20a74109e638813fdcfa173b7286476219
dodiforth <dowoniscool@yahoo.com>
2022-09-20 14:35:14 +0000

    Nonreturning function and Nonreturning method + fatalError (more detail for alter)

--- SWIFT by books/7 Function/function.swift
+++ SWIFT by books/7 Function/function.swift
@@ -213,3 +213,33 @@ func sayGoodbye() -> Void { //Void를 명시해주어도 상관없습니다.
 	print("Good bye")
 }
 sayGoodbye() //Good bye
+
+//종료되지 않는 함수
+/*
+스위프트에는 종료(return)되지 않는 함수가 있습니다. 종료되지 않는다는 의미는 정상적으로 끝나지 않는 함수라는 뜻입니다.
+이를 비반환 함수 Nonreturning function또는 비반환 메서드 Nonreturning method라고 합니다.
+비반환 함수(메서드)는 정상적으로 끝날수 없는 함수입니다. 이 함수를 실행하면 프로세스 동작은 끝났다고 볼 수 있습니다.
+오류를 보고하는 등의 일을 하고 프로세스를 종료해 버리기 때문입니다.
+비반환 함수는 어디서든 호출이 가능하고 guard구문의 else블록에서도 호출할 수 있습니다. 비반환 메서드는
+재정의 할 수 있지만 비반환 타입이라는 것은 변경할 수 없습니다.
+
+비반환 함수(메서드)는 반환 타입을 Never라고 명시해주면 됩니다.
+*/
+//비반환 함수의 정의와 사용
+func crashAndBurn() -> Never {
+	fatalError("Something very, very bad happened")
+}
+
+crashAndBurn() //프로세스 종료 후 오류 보고
+
+func someFunction(isAllIsWell: Bool) {
+	guard isAllIsWell else {
+		print("마을에 도둑이 들었습니다!")
+		crashAndBurn()
+	}
+	print("All is well")
+}
+
+someFunction(isAllIsWell: true) // All is well
+someFunction(isAllIsWell: false) // 마을에 도둑이 들었습니다!
+//프로세스 종료 후 오류 보고
